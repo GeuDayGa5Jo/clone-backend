6d384ab8e493a88acd4286ad7ac9fd7a82a67a2d
강희인 <dwoosung1@gmail.com>
2022-10-29 05:08:12 +0900

    "되는 버전"

--- twiter/src/main/java/com/example/twiter/security/WebConfig.java
+++ twiter/src/main/java/com/example/twiter/security/WebConfig.java
@@ -1,4 +1,21 @@
 package com.example.twiter.security;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    // CORS 설정
+    @Override
+    public void addCorsMappings(CorsRegistry reg) {
+        reg
+                // 전부 허용
+                .addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://localhost:3001")
+                .allowedMethods("GET","POST","PUT","DELETE")
+                .exposedHeaders("Authorization","RefreshToken")
+                .allowCredentials(true);
+    }
 }
