f24e38b2db72ad105af651252200c514ff441d00
강희인 <dwoosung1@gmail.com>
2022-10-29 03:58:32 +0900

    "회원가입 및 로그인"

--- twiter/src/main/java/com/example/twiter/controller/MemberController.java
+++ twiter/src/main/java/com/example/twiter/controller/MemberController.java
@@ -1,4 +1,29 @@
 package com.example.twiter.controller;
 
+import com.example.twiter.dto.Request.MemberRequestDto;
+import com.example.twiter.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
+@RequiredArgsConstructor
 public class MemberController {
+    private final MemberService memberService;
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody MemberRequestDto memberRequestDto) {
+        return memberService.signup(memberRequestDto);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody MemberRequestDto memberRequestDto) {
+        return memberService.login(memberRequestDto);
+    }
+
+
+
 }
